8935aded3cb55b3126dda19381491d880a8bb880
LeoLai <122090246@link.cuhk.edu.cn>
2025-08-20 18:44:18 +0800

    update to the latest submodule

--- scripts/inference_eval/inference_eval_burgers_1d.sh
+++ scripts/inference_eval/inference_eval_burgers_1d.sh
@@ -27,23 +27,23 @@ tasks=("cfl" "beta" "k" "n_space")
 precision_levels=("low" "medium" "high")
 modes=("-z" "")   # "-z" for zero-shot, empty string for iterative
 
-# model_provider="bedrock"
-# models=(
-#  "anthropic.claude-3-5-haiku-20241022-v1:0"
-#  "anthropic.claude-3-5-sonnet-20240620-v1:0"
-#  "anthropic.claude-3-7-sonnet-20250219-v1:0"
-#  "mistral.mistral-large-2402-v1:0"
-#  "meta.llama3-70b-instruct-v1:0"
-#  "amazon.nova-premier-v1:0"
-# )
-
-model_provider="custom_model"
+model_provider="bedrock"
 models=(
+ "anthropic.claude-3-5-haiku-20241022-v1:0"
+ "anthropic.claude-3-5-sonnet-20240620-v1:0"
+ "anthropic.claude-3-7-sonnet-20250219-v1:0"
+ "mistral.mistral-large-2402-v1:0"
+ "meta.llama3-70b-instruct-v1:0"
+ "amazon.nova-premier-v1:0"
+)
+
+# model_provider="custom_model"
+# models=(
 #  "qwen3_0_6b"
 #  "qwen3_8b"
 #  "qwen3_32b" 
- "qwen3_235b_a22b"
-)
+#  "qwen3_235b_a22b"
+# )
 
 # model_provider="openai"
 # models=(
